ff668cbe625f4cfc5ea57717a9e64abef27bc814
Songjie Guo <69680257+songjie-guo@users.noreply.github.com>
2023-04-05 11:36:09 +0800

    Update finetuning_1percent.sh

--- NIH_ChestX-ray/finetuning_1percent.sh
+++ NIH_ChestX-ray/finetuning_1percent.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python3 train.py --name mrm --stage train --model vit_base_patch16 --model_type ViT-B_16 --num_classes 14 \
-    --pretrained_path "../MRM.pth" --dataset_path '../NIH_ChestX-ray' \
+    --pretrained_path "../MRM.pth" --dataset_path '../NIH_ChestX-ray/master' \
     --output_dir "finetuning_outputs/1percent/" --data_volume '1' --num_steps 3000  --eval_batch_size 512 --img_size 224 \
     --learning_rate 3e-2 --warmup_steps 50 --fp16 --fp16_opt_level O2 --train_batch_size 96
 
